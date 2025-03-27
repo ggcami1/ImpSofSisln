01aad91425ff8a18cb29ddc6dc2a73212202a02d
ggcami1 <camigaglez.09@gmail.com>
2025-03-26 18:55:11 -0600

    Add files via upload

---
+++ retos/reto_11/reto_11.py
@@ -0,0 +1,27 @@
+"""
+Usaremos este script para enseñar Python a principiantes absolutos.
+El script es un ejemplo de Fizz-Buzz implementado en Python.
+
+El problema de FizzBuzz:
+Para todos los enteros entre 1 y 99 (incluidos ambos):
+# imprimir fizz para múltiplos de 3
+# imprimir buzz para múltiplos de 5
+# imprimir fizzbuzz para múltiplos de 3 y 5
+"""
+
+def fizzbuzz(max_num):
+    three_mul = 'fizz'
+    five_mul = 'buzz'
+    num1 = 3
+    num2 = 5 
+    for i in range(1,max_num):
+        if i%3==0:
+            print(i,"fizz")
+        elif i%5==0:
+            print(i,"Buzz")     
+        elif i%3==0 and i%5==0:
+            print(i,"fizzbuzz")
+
+#----START OF SCRIPT
+if __name__=='__main__':
+    fizzbuzz(100)
